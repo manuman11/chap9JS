1a2b2805ff6eb1b5892ac16f456aa084b0e25083
Manuman <60324023+manuman11@users.noreply.github.com>
2023-06-20 15:54:24 +0200

    Add files via upload
    voir comment integrer l'image avec le fichier JavaScript.

---
+++ chap9JS/assets/js/index.js
@@ -0,0 +1,33 @@
+/*function produit(){
+    var x=prompt("veuillez entrer un nombre")
+    var y=prompt("veuillez entrer un multiplicateur")
+    alert(x*y)
+}
+    
+
+
+function afficheImg(){
+    var butterfly = document.getElementById(`butterfly`);butterfly.innerHTML ="<img src= `assets/images/papillon.jpg`>";
+    
+}
+            produit()
+            afficheImg();*/
+
+
+
+
+            function produit(){
+                var x=prompt("veuillez entrer un nombre")
+                var y=prompt("veuillez entrer un multiplicateur")
+                alert(x*y)
+            }
+            produit();
+
+            
+            function afficheImg(){
+                var butterfly = document.getElementById('butterfly');
+                butterfly.innerHTML = "<img src='assets/images/papillon.jpg'>";
+            }
+            
+            
+            afficheImg();
