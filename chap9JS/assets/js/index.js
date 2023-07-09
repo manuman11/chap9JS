1e46da89a6181639b5bfd077094c898736a84317
manuman11 <60324023+manuman11@users.noreply.github.com>
2023-07-09 17:28:19 +0200

    exercice 9

--- chap9JS/assets/js/index.js
+++ chap9JS/assets/js/index.js
@@ -1,13 +1,21 @@
+    const review = document.getElementById("view");
+    
     function produit(){
-    let x=prompt("veuillez entrer un nombre")
-    let y=prompt("veuillez entrer un multiplicateur")
-            alert(x*y)         
-    }
+    let x=prompt("veuillez entrer un nombre");
+    let y=prompt("veuillez entrer un multiplicateur");
 
-    function afficheImg(){
-    let butterfly = document.getElementById('butterfly');
-    butterfly.innerHTML = `<img src="assets/images/papillon.jpg">`
+    let result2 = x*x*x;
+    let result = x*y;
+    document.getElementById("result1").innerHTML = ("le cube de "+ x + " est: " + result2);
+    document.getElementById("result2").innerHTML = (x +"x" + y + " est egale à: "+ result);
     }
-            
+  
+    function afficheImg(){
+        document.getElementById('butterfly');
+        butterfly.innerHTML = `<img src="assets/images/papillon.jpg">`
+        }
+
+    
+
     produit();
-    afficheImg();       
+    afficheImg();
